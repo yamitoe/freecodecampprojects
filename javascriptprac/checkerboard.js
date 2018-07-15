6dd4cbc5226eb60ae8dfb72ec9ca608fce8cd35d
Sylph <20277075+yamitoe@users.noreply.github.com>
2018-07-14 17:32:44 -0700

    Better way of writting this

--- javascriptprac/checkerboard.js
+++ javascriptprac/checkerboard.js
@@ -1,36 +1,17 @@
 let test = "";
 let size = 8;
-let alternateValue;
-let choice2;
-for(let col=0; col < size; col++){
 
+for(let col=0; col < size; col++){
   for(let hor=0;hor<size;hor++){
-    
-    //initail value
-  if(col%2==0)
+    if((hor + col) % 2 ==0)
     {
-      alternateValue = " "; 
-      choice2 = "#";
+      test+= " ";
     }
     else
     {
-     	alternateValue= "#"; 
-      choice2=" ";
+      test += "#";
     }
- 
-    //Alternate
-    if(hor % 2 ==0)
-    {
-      test+= alternateValue;
-    }
-    else
-    {
-      test += choice2;
-    }
-    
-    
   }
-  
     test +="\n"
 }
 console.log(test);
